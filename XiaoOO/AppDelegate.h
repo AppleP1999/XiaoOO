8d46c85d9d84a84cca5eed3ef18656d973cfb07c
hf <cyberv@applepen.local>
2017-01-10 17:04:55 +0800

    Initial Commit

---
+++ XiaoOO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiaoOO
+//
+//  Created by hf on 2017/1/10.
+//  Copyright © 2017年 cyw_dj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
